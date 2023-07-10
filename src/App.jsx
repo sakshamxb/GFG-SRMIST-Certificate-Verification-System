d059c0f53b20822f824331c6a288e77aa0a6a753
sakshamxb <saksham.ud@gmail.com>
2023-07-10 06:01:01 +0530

    Verification System

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const dataValues = [
+  {
+    name: "Saksham Bhardwaj",
+    code: "RA2211026030122"
+  },
+  {
+    name: "Trial",
+    code: "123"
+  }
+];
+
+function verifyStudent(value, setResult) {
+  let found = false;
+  for (let i = 0; i < dataValues.length; i++) {
+    if (dataValues[i].code === value) {
+      setResult(`Certificate Verified for ${dataValues[i].name}`);
+      found = true;
+      break;
+    }
+  }
+  
+  if (!found) {
+    setResult('Certificate Unauthorised!');
+  }
+}
+
+function App() {
+  const [verificationResult, setVerificationResult] = useState('');
+
+  function extractCode() {
+    const code = document.getElementById("code");
+    const value = code.value;
+    verifyStudent(value, setVerificationResult);
+  }
+
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="Code" id="code" />
+        <p></p>
+        <button id="verify" onClick={extractCode}>Verify</button>
+      </div>
+        <p id="result">{verificationResult}</p>
+    </>
+  );
+}
+
+export default App;
